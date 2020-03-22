987912d2d68d46b90901523f023d7a96c8d89d26
Anna <frolenkova.a.a@gmail.com>
2020-03-22 11:29:23 +0200

    updated class for cars

---
+++ CarStorage/CarStorage/Cars.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarStorage
+{
+    class Cars
+    {
+        private int _price = 0;
+        public string Color { get; set; }
+        public string Name { get; set; }
+        public int Price { 
+            get => _price;
+            set 
+            {
+                if (value>0)
+                {
+                    _price = value;
+                }
+
+                else
+                {
+                    Console.WriteLine("Incorrect value of the Price");
+                }
+            }
+        }
+
+        internal double GetSale()
+        {
+            var saleSum = new Random().Next(5,10);
+            return Price-(saleSum/100.0*Price); 
+        }
+    }
+}
--- CarStorage/CarStorage/Program.cs
+++ CarStorage/CarStorage/Program.cs
@@ -6,7 +6,25 @@ namespace CarStorage
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Cars [] car = new Cars [2];
+            for (int i = 0; i<car.Length; i++)
+            {
+                car[i] = new Cars();
+                Console.WriteLine("Input Name");
+                car[i].Name = Console.ReadLine();
+
+                Console.WriteLine("Input Color");
+                car[i].Color = Console.ReadLine();
+
+                Console.WriteLine("Input Price");
+                car[i].Price = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Source Price: {car[i].Price}, Price after sale: {car[i].GetSale()}");
+
+            }
+
+            
+
         }
     }
 }
